88502b2e13c7aedd6435ce9fed3af3bef35b098d
izeye <izeye@naver.com>
2020-12-17 22:55:10 +0900

    Use adaptive number of scenarios

--- src/main/java/com/izeye/application/bithumbautotrader/service/AutoTradingRunner.java
+++ src/main/java/com/izeye/application/bithumbautotrader/service/AutoTradingRunner.java
@@ -1,5 +1,6 @@
 package com.izeye.application.bithumbautotrader.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -14,17 +15,31 @@ import com.izeye.application.bithumbautotrader.domain.TradingScenarioFactory;
  * @author Johnny Lim
  */
 @Component
+@Slf4j
 public class AutoTradingRunner implements ApplicationRunner {
+
+	private static final int MAX_EXPECTED_XRP_PRICE = 2_000;
 
 	private final AutoTradingService autoTradingService;
 
-	public AutoTradingRunner(AutoTradingService autoTradingService) {
+	private final BithumbApiService bithumbApiService;
+
+	public AutoTradingRunner(AutoTradingService autoTradingService, BithumbApiService bithumbApiService) {
 		this.autoTradingService = autoTradingService;
+		this.bithumbApiService = bithumbApiService;
 	}
 
 	@Override
 	public void run(ApplicationArguments args) {
-		TradingScenario[] scenarios = TradingScenarioFactory.createLinearScenarios(Currency.XRP, 1, 10);
+		Currency currency = Currency.XRP;
+
+		double currentPrice = this.bithumbApiService.getOrderBook(currency, Currency.KRW).block().getHighestBid();
+		log.info("Current price for {}: {}", currency, currentPrice);
+
+		int numberOfScenarios = (int) (MAX_EXPECTED_XRP_PRICE / currentPrice);
+		log.info("Number of scenarios: {}", numberOfScenarios);
+
+		TradingScenario[] scenarios = TradingScenarioFactory.createLinearScenarios(currency, 1, numberOfScenarios);
 		this.autoTradingService.start(scenarios);
 	}
 
